b4963fb483695c2402888708ec5bc2373d2a6537
Rushi <rushilunagariya7667@gmail.com>
2023-02-27 15:41:57 +0530

    added 13 program

---
+++ 13.py
@@ -0,0 +1,47 @@
+# WAP to find a Multiplication of 2 Matrix (dimension and value should be entered by user)
+
+def mulMat(mat1, mat2, R1, R2, C1, C2):
+    # List to store matrix multiplication result
+    rslt = [[0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]]
+  
+    for i in range(0, R1):
+        for j in range(0, C2):
+            for k in range(0, R2):
+                rslt[i][j] += mat1[i][k] * mat2[k][j]
+  
+    print("Multiplication of given two matrices is:")
+    for i in range(0, R1):
+        for j in range(0, C2):
+            print(rslt[i][j], end=" ")
+        print("\n", end="")
+  
+  
+# Driver code
+
+R1 = 3
+R2 = 3
+C1 = 3
+C2 = 3
+    
+# First matrix. M is a list
+mat1 = [[1, 1, 1],
+        [1, 1, 1],
+        [1, 1, 1]]
+    
+
+# Second matrix. N is a list
+mat2 = [[1, 1, 1],
+        [1, 1, 1],
+        [1, 1, 1]]
+
+if C1 != R2:
+    print("The number of columns in Matrix-1  must be equal to the number of rows in " + "Matrix-2", end='')
+    print("\n", end='')
+    print("Please update MACROs according to your array dimension in #define section", end='')
+    print("\n", end='')
+else:
+    # Call matrix_multiplication function
+    mulMat(mat1, mat2, R1, R2, C1, C2)
